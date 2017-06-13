4bfed45351c3d0e8d8b4448125e5bf1eaa39dc44
Suu Ha <havansuu29111995@gmail.com>
2017-06-13 13:13:13 +0700

    update show delete

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users, only: [:create, :show]
+  resources :users, except: :new
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,13 +1,12 @@
-ActiveRecord::Schema.define(version: 20170612064547) do
-
+ActiveRecord::Schema.define(version: 20170613044249) do
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
-    t.string   "remember_digest"
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
